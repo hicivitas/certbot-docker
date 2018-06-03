ed4c2722eb036d18a399b84c025a39c758511ed5
三三 <oott123@users.noreply.github.com>
2018-06-03 16:43:56 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -4,6 +4,7 @@ mkdir -p /tmp/cert
 cd /tmp/cert
 echo "certbot_dns_dnspod:dns_dnspod_email = \"${DNSPOD_EMAIL}\"" > c.ini
 echo "certbot_dns_dnspod:dns_dnspod_api_token = \"${DNSPOD_TOKEN}\"" >> c.ini
+sleep 99999
 certbot certonly -a certbot-dns-dnspod:dns-dnspod \
     -n --agree-tos --email ${LE_EMAIL} \
     --certbot-dns-dnspod:dns-dnspod-credentials /tmp/cert/c.ini \
